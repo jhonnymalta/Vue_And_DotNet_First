cfa8b73b0f6166d12670853f049f36873a4744b5
Jota Landes <jhonatamalta@hotmail.com>
2022-10-31 19:30:43 -0300

    first commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="SubmitForm">
+      <label for="id">E-mail</label><br />
+      <input type="email" v-model="email" id="email" /><br />
+      <label for="password">password</label><br />
+      <input type="password" v-model="password" id="password" /><br />
+
+      <button>Logar</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios, { Axios } from "axios";
+
+let email = ref();
+let password = ref();
+const SubmitForm = async () => {
+  try {
+    const { data } = await axios.get("https://localhost:7276");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
